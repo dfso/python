4551b3b128690737e230ca303d12bdf9cf5c437a
dfso <denison.new@gmail.com>
2018-03-15 23:16:20 -0400

    atualizando

--- gerador_de_senhas/gerador_de_senhas.py
+++ gerador_de_senhas/gerador_de_senhas.py
@@ -2,11 +2,7 @@
 import gi
 import random
 gi.require_version("Gtk", "3.0")
-<<<<<<< HEAD
 from gi.repository import Gtk, Gdk
-=======
-from gi.repository import Gtk
->>>>>>> 3951cca778187b879279ad68881eed20a8367084
 
 
 # cria a classe do programa
@@ -17,18 +13,12 @@ class GeradorDeSenhas(Gtk.Window):
     builder = Gtk.Builder.new_from_file(glade)
     builder.add_from_file(glade)
 
-<<<<<<< HEAD
     clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
 
     entry_tamanho = builder.get_object("entry_tamanho")
     entry_saida = builder.get_object("entry_saida")
     btn_limpar = builder.get_object("btn_limpar")
     btn_copiar = builder.get_object("btn_copiar")
-=======
-    entry_tamanho = builder.get_object("entry_tamanho")
-    entry_saida = builder.get_object("entry_saida")
-    btn_limpar = builder.get_object("btn_limpar")
->>>>>>> 3951cca778187b879279ad68881eed20a8367084
 
 #==============================================================================
 #   gera a tabela ascII
@@ -47,11 +37,7 @@ class GeradorDeSenhas(Gtk.Window):
             tamanho = int(self.entry_tamanho.get_text())
         except ValueError as e:
             print(str(e))
-<<<<<<< HEAD
             #exit(0)
-=======
-            exit(1)
->>>>>>> 3951cca778187b879279ad68881eed20a8367084
 
         senha = ''
 
@@ -67,13 +53,10 @@ class GeradorDeSenhas(Gtk.Window):
         print("Sua senha: %s" % senha)
         self.entry_saida.set_text(senha)
 
-<<<<<<< HEAD
     def copy_to(self, widget):
         self.clipboard.set_text(self.entry_saida.get_text(), -1)
         print("Senha copiada para clipboard")
 
-=======
->>>>>>> 3951cca778187b879279ad68881eed20a8367084
     def limpar(self, sinal):
         self.entry_saida.set_text("")
 
@@ -84,12 +67,9 @@ class GeradorDeSenhas(Gtk.Window):
 
         btn_gerar = self.builder.get_object("btn_gerar")
         btn_gerar.connect("clicked", self.gerar_senha)
-<<<<<<< HEAD
 
         self.btn_copiar.connect("clicked", self.copy_to)
 
-=======
->>>>>>> 3951cca778187b879279ad68881eed20a8367084
         self.btn_limpar.connect("clicked", self.limpar)
 
         self.entry_tamanho.connect("activate", self.gerar_senha)
