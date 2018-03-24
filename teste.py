d4bdc07eca054096a00bf32d14a5e6406be2ad3f
dfso <denison.new@gmail.com>
2018-03-23 23:20:54 -0400

    commit

--- teste.py
+++ teste.py
@@ -8,8 +8,16 @@ class MySerial(object):
     def __del__(self):
         print("saiu")
 
+    def digaHello(self):
+        """
+        só imprime "olá mundo"
+        """
+        print("olá, mundo!")
+
 
 def main():
      m = MySerial()
+     m.digaHello()
+     print(type(m))
 if __name__ == '__main__':
     main()
