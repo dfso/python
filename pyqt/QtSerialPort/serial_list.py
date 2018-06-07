d336e4f7f53cb73900fe3da721dd9833bbfd61e3
dfso <denison.new@gmail.com>
2018-06-07 08:54:36 -0400

    Organização de projetos

---
+++ pyqt/QtSerialPort/serial_list.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+obter informações em https://doc.qt.io/qt-5.10/qserialportinfo.html
+'''
+
+
+from PyQt5 import QtSerialPort, QtCore
+
+portas = QtSerialPort.QSerialPortInfo.availablePorts()
+
+print("{:-^70}".format("")) #imprime o caracter '-' 70 vezes
+
+for p in portas:
+    print("nome: {} | descrição: {}".format(p.portName(), p.description()))
+
+print("{:-^70}".format("")) #imprime o caracter '-' 70 vezes
+
