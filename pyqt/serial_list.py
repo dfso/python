944a36816b72a2e80bbd2cef9e9dfd164d69263d
dfso <denison.new@gmail.com>
2018-06-07 08:50:48 -0400

    remoção de código não utilizado

--- pyqt/serial_list.py
+++ pyqt/serial_list.py
@@ -16,21 +16,3 @@ for p in portas:
 
 print("{:-^70}".format("")) #imprime o caracter '-' 70 vezes
 
-print("Acessando a porta '/dev/ttyUSB3'")
-arduino = QtSerialPort.QSerialPort("/dev/ttyUSB3")
-
-print(arduino.portName())
-print("porta aberta: {}".format(arduino.isOpen()))
-
-baudRate = 9600
-arduino.setBaudRate(baudRate)
-arduino.open(QtCore.QIODevice.ReadOnly)
-print("porta aberta: {}".format(arduino.isOpen()))
-
-data = QtCore.QByteArray()
-
-while arduino.waitForReadyRead(1):
-    data.append(arduino.readAll())
-
-temp = bytearray(data)
-print(temp.decode("utf8"))
