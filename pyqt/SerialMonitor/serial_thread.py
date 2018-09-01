1a13469d5dd7ad83477677c54486308910c8795c
dfso <denison.new@gmail.com>
2018-08-31 22:59:18 -0400

    Uso de f string e melhoria na lógica de threads

--- pyqt/SerialMonitor/serial_thread.py
+++ pyqt/SerialMonitor/serial_thread.py
@@ -12,41 +12,43 @@ class SerialThread(QtCore.QThread):
     signal = QtCore.pyqtSignal(str)
     port_name = ""
     baud = 0
-    
+
     def __init__(self, port, baud):
         super().__init__()
 
         self.port_name = port
         self.baud = baud
-    
+
         self.arduino = serial.Serial(self.port_name, self.baud)
-        self.arduino.close()
-    
+        print(f'Thread {int(self.currentThreadId())} criada.')
+
     def run(self):
-        print("Thread iniciada.")
+        self.open_port()
+        print(f'Thread {int(self.currentThreadId())} iniciada.')
         while True:
             data = self.arduino.readline().decode().strip()
-            self.signal.emit(str(data)) # pipe
+            self.signal.emit(str(data))  # pipe
             print(data.strip())
-    
+
     def open_port(self):
-        try:
-            self.arduino.open()
-            print("Conexão estabelecida.")
-        except SerialException as ex:
-            print("Um erro ocorreu: {}".format(ex))
-    
+        if not self.arduino.isOpen():
+            try:
+                self.arduino.open()
+                print("Conexão estabelecida.")
+            except SerialException as ex:
+                print(f'Um erro ocorreu: {ex}')
+
     def close_port(self):
         if self.arduino.is_open:
             self.arduino.close()
             print("Porta offline.")
-    
+
     def stop_work(self):
+        print(f'Thread {int(self.currentThreadId())} finalizada.')
         self.terminate()
         self.wait()
         if self.isFinished():
-            print("Thread finalizada.")
+            print(f'Thread {int(self.currentThreadId())} finalizada.')
 
     def send_data(self):
         self.arduino.write(b"A")    # one byte
-        
